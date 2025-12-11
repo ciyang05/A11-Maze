005a8909ac68c92942b79b3990c32e813997a6c3
cyang-05 <chiashiiyang@gmail.com>
2025-12-11 00:04:30 -0500

    solvemaze

--- SolveMaze.java
+++ SolveMaze.java
@@ -5,6 +5,12 @@ import java.util.Arrays;
 
 class SolveMaze {
 
+  /**
+   * reads a file then adds each line into an array
+   * adds each array into a temporary arraylist then converts into array, calling encodeMaze
+   * @param fname
+   * @return
+   */
   public static Scanner readMaze(String fname){
     Scanner file = null;
     try {
@@ -26,17 +32,51 @@ class SolveMaze {
     }
 
     // convert the arraylist tempMaze to an array
-    char[][] maze = tempMaze.toArray(new char[0]);
+    char[][] maze = tempMaze.toArray(new char[tempMaze.size()][]);
     encodeMaze(maze);
 
 
     return file;
   }
 
+  /**
+   * encodes each maze location with the maze content
+   * @param maze
+   */
   public static void encodeMaze(char[][] maze) {
+    // rows of the maze array
     int height = maze.length;
-    int width = maze.
+    // columns of the maze array
+    int width = maze[0].length;
 
+    // creates a new 2D array holding the MazeLocations
+    MazeLocation[][] locations = new MazeLocation[height][width];
+
+    // creates a new 2D array holding the maze contents
+    MazeContents[][] contents = new MazeContents[height][width];
+
+    // iterates through the maze, enumerating the appropriate character,
+    // storing it into contents. Also creates a new maze location, holding that in locations
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        if (maze[i][j] == '#') {
+          locations[i][j] = new MazeLocation(i, j);
+          MazeContents m = MazeContents.WALL;
+          contents[i][j] = m;
+        }
+        if (maze[i][j] == '.') {
+          locations[i][j] = new MazeLocation(i, j);
+          MazeContents n = MazeContents.OPEN;
+          contents[i][j] = n;
+        }
+        if (maze[i][j] == 'S') {
+          locations[i][j] = new MazeLocation(i, j);
+        }
+        if (maze[i][j] == 'F') {
+          locations[i][j] = new MazeLocation(i, j);
+        }
+      }
+    }
   }
   
   public static void main(String[] args) {
